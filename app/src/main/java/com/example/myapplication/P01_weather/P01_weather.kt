03b5ed277572f2f07cd961627089369b9c68bacd
hpmovie <you@example.com>
2020-03-25 18:22:49 +0900

    기상청의 xml데이터 파싱해서 recyclerview에 날씨 정보 보여주기

---
+++ app/src/main/java/com/example/myapplication/P01_weather/P01_weather.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication.P01_weather
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_p01_weather.*
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.net.URL
+
+class P01_weather : AppCompatActivity() {
+    data class MyWeatherData(var hour:Int, var day:Int, var temp:Float, var wfkor:String )
+    val myWeatherList = ArrayList<MyWeatherData>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p01_weather)
+        WeatherTask().execute("http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1153052000")
+        myWeatherView.layoutManager = LinearLayoutManager(this)
+    }
+
+    inner class WeatherTask: AsyncTask<String, Unit, String>() {
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            myWeatherView.adapter = WeatherAdapter(myWeatherList)
+        }
+        override fun doInBackground(vararg params: String?): String {
+            val path = params[0]!!
+            val url = URL(path)
+
+            val factory = XmlPullParserFactory.newInstance()
+            val xpp = factory.newPullParser()
+            xpp.setInput(url.openStream(), "utf-8")
+
+            //var bRead = false;
+            var res = ""
+            var eventType = xpp.eventType
+            var startTag = ""
+            var data = MyWeatherData(0,0, 0.0f,"")
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+                if(eventType == XmlPullParser.START_TAG)
+                    startTag = xpp.name
+                else if(eventType == XmlPullParser.TEXT) {
+                    when (startTag) {
+                        "hour" -> {
+                            val hour = xpp.text.toInt()
+                            data = MyWeatherData(0,0, 0.0f,"")
+                            myWeatherList.add(data)
+                        }
+                        "day" -> { data.day = xpp.text.toInt()}
+                        "wfKor" -> { data.wfkor = xpp.text}
+                        "temp" -> { data.temp = xpp.text.toFloat()}
+                    }
+                    startTag=""
+                }
+
+                eventType = xpp.next()
+            }
+            return res;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/P01_weather/WeatherAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.myapplication.P01_weather
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import com.example.myapplication.T10_Recyclerview.MyRecyclerAdapter
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class WeatherAdapter(val myList:ArrayList<P01_weather.MyWeatherData>): RecyclerView.Adapter<WeatherAdapter.WeatherVH>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherVH {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_custom_listview, parent, false)
+
+        return WeatherVH(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return myList.size
+    }
+
+    override fun onBindViewHolder(holder: WeatherVH, position: Int) {
+        val data = myList[position]
+        holder.bind(data)
+
+    }
+
+    class WeatherVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
+        val tvDesc: TextView = itemView.findViewById(R.id.tvDesc)
+        val itemImageView: ImageView = itemView.findViewById(R.id.itemImageView)
+
+        fun bind(data:P01_weather.MyWeatherData) {
+            val cal = Calendar.getInstance()
+            cal.add(Calendar.DATE, data.day)
+            val df = SimpleDateFormat("yyyy-MM-dd")
+            val strDate = df.format(cal.time)
+            tvTitle.text = "${strDate} ${data.hour}시"
+            tvDesc.text = "${data.wfkor} ${data.temp}c"
+            val res =R.mipmap.ic_launcher
+            /*when{
+                data.wfKor.contains("구름")-> R.drawable.ic_cloud_black_24dp
+                data.wfKor.contains("비")-> R.drawable.ic_umbrella_black_24dp
+                data.wfKor.contains("맑음")-> R.drawable.ic_wb_sunny_black_24dp
+                else -> R.mipmap.ic_launcher
+            }*/
+            itemImageView.setImageResource(res)
+        }
+    }
+
+
+}
