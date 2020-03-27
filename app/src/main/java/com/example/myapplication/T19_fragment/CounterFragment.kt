e0034fffda9d91d5adeb79c39af13b5f5b27d33f
hpmovie <you@example.com>
2020-03-27 11:03:40 +0900

    fragment

---
+++ app/src/main/java/com/example/myapplication/T19_fragment/CounterFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.T19_fragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.fragment_counter2.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CounterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.fragment_counter2, container, false)
+        btnIncrease.setOnClickListener{
+            Log.d("CounterFragment", "click")
+        }
+        return v
+    }
+
+}
